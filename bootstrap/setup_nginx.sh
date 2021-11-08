1677da08d61f40835e5c946895c6599bf972b6f2
David Rahaeuser <david.rahaeuser@fractiunate.de>
2021-11-08 15:39:40 +0100

    bootstrap update

--- bootstrap/setup_nginx.sh
+++ bootstrap/setup_nginx.sh
@@ -1,7 +1,10 @@
 sudo yum -y update
-sudo yum -y install maven nginx
+sudo amazon-linux-extras install -y java-openjdk11  nginx1.12
 
-
+# Install Maven on AMZN-LINUX 2
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
 
 # kill processes on {PORT} and re-run spring app on that port
 # TODO needs better solution: https://stackoverflow.com/questions/17385794/how-to-get-the-process-id-to-kill-a-nohup-process
@@ -15,6 +18,7 @@ nohup java -jar target/df-jspring-brewery-0.0.1-SNAPSHOT.jar &
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
 
 # Config Nginx
+mv /etc/nginx /etc/nginx-backup #Backup Config
 sudo service nginx start
 sudo cp ./nginx_config /etc/nginx/sites-enabled/default
 sudo cp ./ssl-params.conf /etc/nginx/snippets/ssl-params.conf
