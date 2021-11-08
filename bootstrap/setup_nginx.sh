24f22d22912242a647e1b152a689790238ee605d
David Rahaeuser <david.rahaeuser@fractiunate.de>
2021-11-08 16:15:57 +0100

    bootstrap update

--- bootstrap/setup_nginx.sh
+++ bootstrap/setup_nginx.sh
@@ -11,8 +11,13 @@ sudo yum install -y apache-maven
 kill -9 $(lsof -t -i:8080)
 
 cd ../
+# Set JAVA 11 for this project
+java_11_path=$(update-alternatives --display java | grep 11 | head -n 1)
+java_11_path="${java_11_path##* }"
+sudo alternatives --set java $java_11_path
+
 # Build Spring App & run in Background
-sudo  mvn -DskipTests install
+sudo mvn -DskipTests install
 sudo nohup java -jar target/df-jspring-brewery-0.0.1-SNAPSHOT.jar &
 cd bootstrap/
 
@@ -20,7 +25,7 @@ cd bootstrap/
 sudo mkdir -p /etc/ssl/private/ && sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=DE/ST=Bamberg/L=Bamberg/O=Fractiunate/OU=IT/CN=fractiunate.com"
 
 # Config Nginx
-mv /etc/nginx /etc/nginx-backup #Backup Config
+sudo mv /etc/nginx /etc/nginx-backup #Backup Config
 sudo service nginx start
 sudo mkdir -p /etc/nginx/sites-enabled && cp ./nginx_config /etc/nginx/sites-enabled/default
 sudo mkdir -p /etc/nginx/snippets/ && cp ./ssl-params.conf /etc/nginx/snippets/ssl-params.conf
