e806dc01c41a4989cdeca2dd57ef8d19041e97c2
David Rahaeuser <david.rahaeuser@fractiunate.de>
2021-11-16 11:47:57 +0100

    updated nginx_config

--- bootstrap/setup_nginx.sh
+++ bootstrap/setup_nginx.sh
@@ -14,14 +14,14 @@ cd ../
 # Set JAVA 11 for this project
 java_11_path=$(update-alternatives --display java | grep 11 | head -n 1 | xargs)
 
-# is path already set to Java 11?
-if [[ ${java_11_path} == "link currently points to"* ]]; then 
-    java_11_path=$(echo "${java_11_path%%* }")
-    echo "INFO: ${java_11_path}"
-else
-    java_11_path=$(echo "${java_11_path}" | sed -e 's/\s.*$//')
-    sudo alternatives --set java $java_11_path
-fi;
+    # is path already set to Java 11?
+    if [[ ${java_11_path} == "link currently points to"* ]]; then 
+        java_11_path=$(echo "${java_11_path%%* }")
+        echo "INFO: ${java_11_path}"
+    else
+        java_11_path=$(echo "${java_11_path}" | sed -e 's/\s.*$//')
+        sudo alternatives --set java $java_11_path
+    fi;
 
 
 # Build Spring App & run in Background
